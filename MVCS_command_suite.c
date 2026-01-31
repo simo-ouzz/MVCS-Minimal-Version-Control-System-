98ad53f558cb945ced91eba205d3a06fa979a3a4
unknown <mohammedouazini@gmail.com>
2026-01-31 01:01:42 +0100

    completed the objects structure and made the first CLI command

---
+++ MVCS_command_suite.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <windows.h>
+
+
+int main(int argc, char *argv[]) {
+    if (argc > 1 && strcmp(argv[1], "init") == 0) {
+        printf("MVCS repository initialized.\n");
+        CreateDirectory(".mvcs", NULL);
+        CreateDirectory(".//.mvcs//objects", NULL);
+        CreateDirectory(".//.mvcs//refs", NULL);
+        CreateDirectory(".//.mvcs//refs//heads", NULL);
+        FILE *headFile = fopen(".//.mvcs//HEAD", "w");
+        if (headFile != NULL) {
+            fprintf(headFile, "ref: refs/heads/main\n");
+            fclose(headFile);
+        }
+        return 0;
+    } else {
+        printf("MVCS: no command given.\n");
+        return 0;
+    }
+   
+}
--- VCS_Structure.c
+++ VCS_Structure.c
@@ -1,32 +1,52 @@
-#include <cstddef>
+#include <stddef.h>
+#include <stdint.h>
 
-struct 
-{
-    char *hash;
-    char *author;
-    char *date;
-    char *message;
-}; typedef struct Commit;
+#define HASH_SIZE 20        // SHA-1 binary size
+#define HASH_HEX_SIZE 41    // 40 hex + null
+#define MAX_NAME 256
 
-struct
-{
-    char *name;
-    struct tree **subtrees;
-    int subtree_count;
-    struct commit **commits;
-    int commit_count;
-}; typedef struct  Tree;
+/* ---------- Forward Declarations ---------- */
+typedef struct Tree Tree;
 
-struct repository
-{
-    char *name;
-    struct tree *root_tree;
-}; typedef struct repository Repository;
+/* ---------- Entry Type ---------- */
+typedef enum {
+    ENTRY_FILE,
+    ENTRY_DIR
+} EntryType;
 
-struct blob
-{
-    char *filename;
-    char *content;
+/* ---------- Tree Entry ---------- */
+typedef struct {
+    char name[MAX_NAME];          // filename
+    char mode[8];                 // "100644", "040000", etc.
+    
+    uint8_t hash[HASH_SIZE];      // binary SHA-1
+    
+    EntryType type;               // file or dir
+    
+    Tree *subtree;                // only valid if ENTRY_DIR
+} TreeEntry;
+
+/* ---------- Tree Object ---------- */
+struct Tree {
+    TreeEntry *entries;
+    size_t entry_count;
+};
+
+/* ---------- Commit Object ---------- */
+typedef struct {
+    uint8_t hash[HASH_SIZE];          // commit's own hash
+    
+    uint8_t tree_hash[HASH_SIZE];     // root tree hash
+    uint8_t parent_hash[HASH_SIZE];   // parent commit hash
+    
+    char author[100];
+    char message[256];
+    char timestamp[32];
+} Commit;
+
+/* ---------- Blob Object ---------- */
+typedef struct {
+    uint8_t hash[HASH_SIZE];  // blob hash
     size_t size;
-}; typedef struct blob Blob;
-
+    uint8_t *data;            // raw file content
+} Blob;
